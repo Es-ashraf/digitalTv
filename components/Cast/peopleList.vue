ad65d2bcff821946e4e0b7efae5002273f86cdd7
eslamashraf-97 <e01095097908@gmail.com>
2022-12-20 11:26:27 +0200

    start

---
+++ components/Cast/peopleList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="people-wrapper">
+    <person v-for="person in people" :key="person.id" :person="person"></person>
+  </div>
+</template>
+
+<script>
+import person from './Person.vue'
+
+export default {
+  components: { person },
+  props: ['people'],
+}
+</script>
+
+<style>
+</style>
---
+++ components/UI/AppLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <img src="~/assets/imgs/logo.png" alt="" width="150" />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
